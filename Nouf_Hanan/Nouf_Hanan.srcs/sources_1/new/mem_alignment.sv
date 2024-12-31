c0b541ed50787e727b782ead7e457b5561a4626d
NOUF <n8ouf@hotmail.com>
2024-12-31 14:34:11 +0300

    lab7

---
+++ Nouf_Hanan/Nouf_Hanan.srcs/sources_1/new/mem_alignment.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 31.12.2024 11:36:42
+// Design Name: 
+// Module Name: mem_alignment
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mem_alignment(
+
+    );
+endmodule
